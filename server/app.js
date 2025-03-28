53900e7aefadd5e82b22ada0cc01704825b6ddfd
Soumyajit Sarkar <soumyajit.sarkar@hyland.com>
2025-03-28 17:17:33 +0530

    Render deploy

--- server/app.js
+++ server/app.js
@@ -4,11 +4,11 @@ const bodyParser = require('body-parser');
 const dbConnection = require('./database/database');
 const homeRouter = require('./Routes/homeRoutes');
 const path = require('path');
+const { fileURLToPath } = require('url');
 const ProductsPictures = require('./database/models/productPics');
 require('dotenv').config();
 
 const PORT = process.env.PORT;
-const CLIENTPORT = process.env.CLIENTPORT;
 
 const app = express();
 
@@ -17,17 +17,12 @@ app.options('*', cors());
 app.use(bodyParser.json());
 app.use(homeRouter);
 
+app.use(express.static(path.join(__dirname, '../client/dist/rickshaw-website')));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/dist/rickshaw-website/index.html'));
+});
+
 app.listen(PORT, () => {
 	console.log('SERVER started on port', PORT);
 });
-
-// const clientApp = express();
-// clientApp.use(express.static(path.join(__dirname, '../client/dist/rickshaw-website')));
-
-// clientApp.get('/*', (req, res) => {
-// 	res.sendFile(path.join(__dirname, '../client/dist/rickshaw-website/index.html'));
-// });
-
-// clientApp.listen(CLIENTPORT, () => {
-// 	console.log('CLIENT started on port', CLIENTPORT);
-// });
